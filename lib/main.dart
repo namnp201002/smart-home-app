99381417616362ba29d6eaab245c598d1dd2ea71
namnp201002 <namnp.201002@gmail.com>
2024-05-16 20:26:31 +0700

    First Smart Home App

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:smart_home/firebase_options.dart';
+import 'package:smart_home/screens/home.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: const [
+        // ChangeNotifierProvider(create: (context) => SignInProvider()),
+      ],
+      child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Smart Home',
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+            useMaterial3: true,
+          ),
+          home: const Scaffold(
+            body: Home(),
+          )),
+    );
+  }
+}
---
+++ lib/screens/home.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child:  Text("Smart Home App", style: TextStyle(
+        
+      ),)
+    );
+  }
+}
